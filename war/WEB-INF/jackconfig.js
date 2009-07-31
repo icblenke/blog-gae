1777c60e958fbf7603368c289842515ad0fdc4ed
George Moschovitis <george.moschovitis@gmail.com>
2009-07-31 11:02:11 +0300

    Removed unneeded xslt jars.

--- war/WEB-INF/jackconfig.js
+++ war/WEB-INF/jackconfig.js
@@ -1,7 +1,7 @@
 require.paths.unshift("WEB-INF/src");
 
-require("oop");
 require("nitro");
+require("oop");
 require("dateutils");
 	
 var File = require("jack/file").File, 
@@ -15,17 +15,14 @@ var Dispatch = require("nitro/dispatch").Dispatch,
 	Normalize = require("nitro/normalize").Normalize,
 	Errors = require("nitro/errors").Errors,
 	SessionManager = require("nitro/sessionmanager").SessionManager,
-	Render = require("nitro/render").Render,
-	Layout = require("nitro/utils/layout").Layout;
-	
+	Render = require("nitro/render").Render;
+		
 var Wrap = require("./src/wrap").Wrap;
 
 CONFIG.root = "WEB-INF/src/root";
 CONFIG.templateRoot = "WEB-INF/src/templates";
 
-var layout = new Layout();
-
-exports.app = MethodOverride(Normalize(SessionManager(Render(Wrap(Dispatch()), layout), "sessionsecret")));
+exports.app = MethodOverride(Normalize(SessionManager(Render(Wrap(Dispatch())), "sessionsecret")));
 
 exports.development = function(app) {
 	throw new Error("You cannot launch a Google App Engine application with jackup");
