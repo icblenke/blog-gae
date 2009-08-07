4c9edc43e1321a72eb2b32525d688817755d67b8
George Moschovitis <george.moschovitis@gmail.com>
2009-08-07 13:54:48 +0300

    Minor.

--- war/WEB-INF/src/root/screen.js
+++ war/WEB-INF/src/root/screen.js
@@ -3,9 +3,8 @@
 // Mainly to demonstrate what is possible.
 
 exports.GET = function(env) {
-    if (env["CONTENT_TYPE"] == "text/css")
-        return [200, { "Cache-Control": "public; max-age=8640000" }, {
-            gray: "#eee"        
-        }];
+    return [200, { "Cache-Control": "public; max-age=8640000" }, {
+        gray: "#eee"        
+    }];
 }
 
