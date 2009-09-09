e44224f343da5c9a377f42f2d0574d6d10252f42
George Moschovitis <george.moschovitis@gmail.com>
2009-09-09 11:57:07 +0300

    Kindmap to be used in the datastore admin.

---
+++ war/WEB-INF/src/kindmap.js
@@ -0,0 +1,7 @@
+var kindMap = exports.kindMap = {
+    "Article": require("content/article").Article,
+    "Category": require("content/category").Category,
+    "Comment": require("content/comment").Comment,
+    "Tag": require("content/tag").Tag
+}
+
